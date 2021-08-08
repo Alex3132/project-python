7964e6a8f1f9e4de6c8df151029118e6cc226f04
Alex3132 <alexis.zueras.pro@gmail.com>
2021-08-08 18:17:26 +0200

    new fonction

--- number_guess.py
+++ number_guess.py
@@ -1,13 +1,15 @@
 import random
+def quitter():
+    if top_of_range <=0:
+        print("Please type a number larger than 0 next time")
+        quit()
 
 top_of_range = input("Type a number: ")
 
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
+    quitter()
 
-    if top_of_range <=0:
-        print("Please type a number larger than 0 next time")
-        quit()
 else:
     print("Please type a number next time")
     quit()
